d2c6c8c107d1930915085bac885b6e8a5478eb65
Zachary Burns <zacharykjburns@gmail.com>
2022-02-14 07:54:21 -0500

    Footer created and issue found
    Site cannot load images unless in wordpress

--- themes/tbtcan/footer.php
+++ themes/tbtcan/footer.php
@@ -52,3 +52,5 @@ Thank you. TableTop Canada
 
 </body>
 </html>
+
+<!-- Footer created  -->
--- themes/tbtcan/inc/woocommerce.php
+++ themes/tbtcan/inc/woocommerce.php
@@ -12,7 +12,5 @@ function tbtcan_use_block_editor_for_post_type($use_block_editor, $post_type){
 }
 add_filter('use_block_editor_for_post_type', 'tbtcan_use_block_editor_for_post_type', 10, 2 );
 
-add_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
-
 
 // add_action( 'woocommerce_after_add_to_cart_button' );
