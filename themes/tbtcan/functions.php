97dc97d034abce7ad07a8368e9e514089c57d950
Zachary Burns <zacharykjburns@gmail.com>
2022-02-13 21:16:52 -0500

    Changed Menu ID
    Changed menu-1 to menu-primary in both functions and header

--- themes/tbtcan/functions.php
+++ themes/tbtcan/functions.php
@@ -49,7 +49,7 @@ function tbtcan_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'tbtcan' ),
+			'menu-primary' => esc_html__( 'Primary', 'tbtcan' ),
 		)
 	);
 
