48fc46ad7187bfd5c747cfd7f495aa801e6c24b9
Zachary Burns <zacharykjburns@gmail.com>
2022-02-13 21:04:53 -0500

    Custom Theme Initialization
    Deleted all useless files, set up git hub, changed all paths to correct paths. Set up theme on running site.

---
+++ themes/tbtcan/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Table_Top_Canada
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tbtcan' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'tbtcan' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tbtcan' ), 'tbtcan', '<a href="https://zacharyburnswebdesign.ca">Zachary Burns</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
